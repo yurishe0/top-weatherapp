25e032952226f0491a024815abb73f39bc1645f5
yurishe0 <yurishe00@gmail.com>
2023-10-27 19:01:03 +0200

    Add DOM selectors for info containers in HTML

--- src/index.js
+++ src/index.js
@@ -4,6 +4,14 @@ import { UI } from "./UI";
 const input = document.querySelector("#location");
 const button = document.querySelector("button");
 
+// data containers
+const weatherInfoMain = document.querySelector("#weather-info__main");
+const headerContainer = document.querySelector("#header-container");
+const timeContainer = document.querySelector("#time-container");
+const weatherInfoOther = document.querySelector("#weather-info__other");
+const forecastContainer = document.querySelector("#forecast-container");
+const creditsContainer = document.querySelector("#credits-container");
+
 button.addEventListener("click", async () => {
     const data = await Data.getWeatherData(input.value);
     UI.updateBackground("sunny");
