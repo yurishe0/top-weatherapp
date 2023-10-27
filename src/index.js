5516129a89fbf4291208dce9ac0182d64e10aae3
yurishe0 <yurishe00@gmail.com>
2023-10-27 18:41:06 +0200

    Remove the testing paragraph

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("Test");
+import { Data } from "./Data";
+import { UI } from "./UI";
+
+const input = document.querySelector("#location");
+const button = document.querySelector("button");
+
+button.addEventListener("click", async () => {
+    const data = await Data.getWeatherData(input.value);
+    UI.updateBackground("sunny");
+})
